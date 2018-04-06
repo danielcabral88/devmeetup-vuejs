6d09a87c57f67e64fbe6e105d63779c1fab0d487
Daniel Cabral <daniel.cabral@armateus.com.br>
2018-04-06 18:07:41 -0300

    implementação de upload de imagens no firebase

---
+++ src/components/Shared/Alert.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-alert error dismissible @input="onClose">
+      This is a success alert that is closeable.
+  </v-alert>
+</template>
+
+<script>
+export default {
+  method: {
+    onClose () {
+      this.$emit('dismissed')
+    }
+  }
+}
+</script>
